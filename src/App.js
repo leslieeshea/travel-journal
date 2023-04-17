fd6b43dcc6ba90af216a7da31c4d938e82e44f56
leslieshea <lshea@audigy.com>
2023-04-17 16:24:53 -0700

    Created Navbar component and rendering in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
-    <h1>Hello world</h1>
+    <Navbar />
   )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Navbar() {
+	return (
+		<header>
+			<nav>
+				<h1 className='title'>my travel journal</h1>
+			</nav>
+		</header>
+	)
+}
