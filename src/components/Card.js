cbe9137ea163c7fa0fc9ec2c55f1617517147104
leslieshea <lshea@audigy.com>
2023-04-17 17:28:26 -0700

    Created data.js and updated class names

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,14 +5,14 @@ export default function Card() {
 		<div className='card'>
 			<div className='card--image'></div>
 			<div className='card--details'>
-				<span className='card--city'>ISTANBUL</span>
+				<span className='card--location'>ISTANBUL</span>
 				<a
 					href='https://www.google.com/maps/d/viewer?mid=1izAC_NU-T8opgp_MeLgji5_uDhE&hl=en_US&ll=41.00850000000001%2C28.979283999999996&z=17'
 					className='card--map'
 				>
 					View on Google Maps
 				</a>
-				<h2 className='card--place'>Hagia Sophia</h2>
+				<h2 className='card--title'>Hagia Sophia</h2>
 				<p className='card--dates'>12 May, 2016 - 24 May, 2016</p>
 				<p className='card--description'>
 					Istanbul is home to several churches belonging to the Byzantine architecture and Hagia Sophia happens to be the most significant Byzantine churches.
---
+++ src/data.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+	{
+		title: "Taipei 101",
+		location: "Taiwan",
+		googleMapsUrl: "https://www.google.com/maps/d/viewer?mid=1U6fb_2PgtskZbzOZHIQ27YKLy5o&hl=en&ll=25.033738999999997%2C121.564322&z=17",
+		startDate: "9 Oct, 2019",
+		endDate: "22 Oct, 2019",
+		description: "TAIPEI is mnemonic for Technology, Art, Innovation, People, Environment, and Identity. 101 represents the concept of striving for beyond perfection.",
+		imageUrl: "https://images.unsplash.com/photo-1601534621622-8587a8a0da11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+	},
+	{
+		title: "Hagia Sophia",
+		location: "Istanbul",
+		googleMapsUrl: "https://www.google.com/maps/d/viewer?mid=1izAC_NU-T8opgp_MeLgji5_uDhE&hl=en_US&ll=41.00850000000001%2C28.979283999999996&z=17",
+		startDate: "12 May, 2016",
+		endDate: "24 May, 2016",
+		description: "Istanbul is home to several churches belonging to the Byzantine architecture and Hagia Sophia happens to be the most significant Byzantine churches. Hagia Sophia in Greek (Ayasofya in Turkish) happens as the universally acknowledged and the most visited museums in the world.",
+		imageUrl: "https://images.unsplash.com/photo-1621164870333-7931835f05de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+	},
+	{
+		title: "Trevi Fountain",
+		location: "Italy",
+		googleMapsUrl: "https://www.google.com/maps/d/viewer?mid=1nfLdUeQthGEERiCr_TuaMs8pzlk&hl=en_US&ll=41.900623999999986%2C12.483076000000016&z=17",
+		startDate: "7 Sep, 2000",
+		endDate: "22 Sep, 2000",
+		description: "It may or may not be the most beautiful fountain in Rome but it is without doubt the most famous. The imaginative concept, the theatrical composition, the sober and imposing beauty of the sculptured marble figures make it a true masterpiece both of sculpture and of architecture. Pietro da Cortona and above all Bernini, who began the undertaking, both had a hand in the project.",
+		imageUrl: "https://images.unsplash.com/photo-1584999872814-569a6b02a2b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80"
+	}
+]
