a376d3aa2db43c6f098f41c74edc32ff0ca18ebd
leslieshea <lshea@audigy.com>
2023-04-18 09:01:17 -0700

    Added more data

--- src/data.js
+++ src/data.js
@@ -2,12 +2,21 @@
 export default [
 	{
 		title: "Taipei 101",
-		location: "Taiwan",
+		location: "Taipei",
 		googleMapsUrl: "https://www.google.com/maps/d/viewer?mid=1U6fb_2PgtskZbzOZHIQ27YKLy5o&hl=en&ll=25.033738999999997%2C121.564322&z=17",
 		startDate: "9 Oct, 2019",
-		endDate: "22 Oct, 2019",
+		endDate: "19 Oct, 2019",
 		description: "TAIPEI is mnemonic for Technology, Art, Innovation, People, Environment, and Identity. 101 represents the concept of striving for beyond perfection.",
 		imageUrl: "https://images.unsplash.com/photo-1601534621622-8587a8a0da11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+	},
+	{
+		title: "Rockefeller Center",
+		location: "New York",
+		googleMapsUrl: "https://www.google.com/maps/d/viewer?mid=1QiF_3xgxcXDtN1AVLJEHb0c689o&hl=en&ll=40.7584370013142%2C-73.97794199999998&z=19",
+		startDate: "19 Oct, 2019",
+		endDate: "24 Oct, 2019",
+		description: "Rockefeller Center is a large complex consisting of 19 commercial buildings covering 22 acres (89,000 m2) between 48th Street and 51st Street in the Midtown Manhattan neighborhood of New York City. The 14 original Art Deco buildings, commissioned by the Rockefeller family, span the area between Fifth Avenue and Sixth Avenue, split by a large sunken square and a private street called Rockefeller Plaza.",
+		imageUrl: "https://images.unsplash.com/photo-1454686492631-2ce80cbec3a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"
 	},
 	{
 		title: "Hagia Sophia",
@@ -20,7 +29,7 @@ export default [
 	},
 	{
 		title: "Trevi Fountain",
-		location: "Italy",
+		location: "Rome",
 		googleMapsUrl: "https://www.google.com/maps/d/viewer?mid=1nfLdUeQthGEERiCr_TuaMs8pzlk&hl=en_US&ll=41.900623999999986%2C12.483076000000016&z=17",
 		startDate: "7 Sep, 2000",
 		endDate: "22 Sep, 2000",
